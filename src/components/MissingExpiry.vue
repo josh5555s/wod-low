e41d7f099d1f3aae48e4999bdc37f095125ea76a
BuildTools <unconfigured@null.spigotmc.org>
2022-05-11 09:31:50 -0700

    now rendering like low counts

--- src/components/MissingExpiry.vue
+++ src/components/MissingExpiry.vue
@@ -1,44 +1,144 @@
 <template>
   <div>
     <ul v-for="store in stores" :key="store">
+      <li @click="toggleExpandedStore(store)">
+        <a>
+          {{ store }}
+        </a>
+      </li>
+      <ul
+        v-for="productType in productTypes(store)"
+        :key="productType"
+        class="product-list"
+      >
+        <li
+          v-if="isStoreExpanded(store)"
+          @click="toggleExpandedProductType(store, productType)"
+        >
+          <a>
+            {{ productType }}
+            ({{ items(store, productType).length }})
+          </a>
+        </li>
+        <ul
+          v-for="(item, i) in items(store, productType)"
+          :key="item"
+          class="item-list"
+        >
+          <div
+            v-if="
+              isStoreExpanded(store) && isProductExpanded(store, productType)
+            "
+            @click="toggleShowFullItem(store, productType, i)"
+          >
+            <a>
+              <li>Name: {{ item.productName }}</li>
+              <div v-if="expansions[store][productType][i].full === true">
+                <li>Brand: {{ item.farm }}</li>
+                <li>Price: ${{ item.price }}</li>
+                <li>Feel: {{ item.type }}</li>
+              </div>
+            </a>
+          </div>
+        </ul>
+      </ul>
+    </ul>
+  </div>
+  <!-- <div>
+    <ul v-for="store in stores" :key="store">
       <li>
         {{ store }}
         <ul v-for="type in productTypes" :key="type">
-          <li class="hidden">
-            <span class="hidden">
+          <li>
+            <span>
               {{ type }}
             </span>
             <ul v-for="item in items(store, type)" :key="item">
-              <span class="hidden">
-                <li>{{ item.productName }}</li>
+              <span>
+                <li>
+                  {{ item.productName }}
+                </li>
               </span>
             </ul>
           </li>
         </ul>
       </li>
     </ul>
-  </div>
+  </div> -->
 </template>
 
 <script>
 export default {
-  props: ["inventory", "stores", "productTypes"],
+  props: ["inventory"],
   data() {
-    return {};
+    return {
+      expansions: {},
+    };
   },
-  computed: {},
-  methods: {
-    items(store, productType) {
-      return this.inventory[store][productType];
+  computed: {
+    stores() {
+      return Object.keys(this.inventory);
     },
   },
-  created() {},
+  methods: {
+    productTypes(store) {
+      return Object.keys(this.inventory[store]);
+    },
+    items(store, productType) {
+      return this.inventory[store][productType].filter((item) => {
+        console.log(item.expiration);
+        return item.expiration === "";
+      });
+    },
+    initExpansionsObj() {
+      this.expansions = JSON.parse(JSON.stringify(this.inventory));
+    },
+    initExpandedProps() {
+      this.stores.forEach((store) => {
+        this.expansions[store].expanded = {};
+        this.expansions[store].expanded.store = false;
+        this.productTypes(store).forEach((productType) => {
+          this.expansions[store].expanded[productType] = false;
+        });
+      });
+    },
+    isStoreExpanded(store) {
+      return this.expansions[store].expanded.store ? true : false;
+    },
+    isProductExpanded(store, productType) {
+      return this.expansions[store].expanded[productType] ? true : false;
+    },
+    toggleExpandedStore(store) {
+      return (this.expansions[store].expanded.store =
+        !this.expansions[store].expanded.store);
+    },
+    toggleExpandedProductType(store, productType) {
+      return (this.expansions[store].expanded[productType] =
+        !this.expansions[store].expanded[productType]);
+    },
+    toggleShowFullItem(store, productType, i) {
+      if ("full" in this.expansions[store][productType][i] == false) {
+        this.expansions[store][productType][i].full = true;
+        console.log("toggleShowFullItem", store, productType, i);
+      } else {
+        this.expansions[store][productType][i].full =
+          !this.expansions[store][productType][i].full;
+      }
+    },
+  },
+  created() {
+    this.initExpansionsObj();
+    this.initExpandedProps();
+  },
   mounted() {},
+  watch: {
+    inventory() {
+      this.initExpansionsObj();
+      this.initExpandedProps();
+    },
+  },
 };
 </script>
 
 <style>
-.hidden {
-  display: none;
-}
 </style>
